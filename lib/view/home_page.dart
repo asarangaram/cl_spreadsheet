420882337169efd9ecdc27492c8e93f3c392a312
Ananda Sarangaram <4833102+asarangaram@users.noreply.github.com>
2025-10-30 11:24:13 +0530

    Have column in home page

--- lib/view/home_page.dart
+++ lib/view/home_page.dart
@@ -16,87 +16,40 @@ class HomePage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MyScaffold(
-      child: Center(
-        child: ShadButton.outline(
-          child: const Text('New'),
-          onPressed: () async {
-            final sheet = await showShadDialog<SheetProperties?>(
-              context: context,
-              barrierColor: Colors.transparent,
-              builder: (context) => ShadDialog.alert(
-                gap: 0,
-                backgroundColor: Colors.transparent,
+      child: Column(
+        children: [
+          Expanded(
+            child: Center(
+              child: ShadButton.outline(
+                child: const Text('New'),
+                onPressed: () async {
+                  final sheet = await showShadDialog<SheetProperties?>(
+                    context: context,
+                    barrierColor: Colors.transparent,
+                    builder: (context) => ShadDialog.alert(
+                      gap: 0,
+                      backgroundColor: Colors.transparent,
 
-                shadows: [],
-                border: BoxBorder.all(color: Colors.transparent),
-                child: SizedBox(
-                  width: 375,
-                  child: NewSheetForm(
-                    onClose: () => Navigator.of(context).pop(null),
-                    onSubmit: ({required sheetProperties}) =>
-                        Navigator.of(context).pop(sheetProperties),
-                  ),
-                ),
+                      shadows: [],
+                      border: BoxBorder.all(color: Colors.transparent),
+                      child: SizedBox(
+                        width: 375,
+                        child: NewSheetForm(
+                          onClose: () => Navigator.of(context).pop(null),
+                          onSubmit: ({required sheetProperties}) =>
+                              Navigator.of(context).pop(sheetProperties),
+                        ),
+                      ),
+                    ),
+                  );
+                  if (sheet != null) {
+                    print("Sheet : $sheet");
+                  }
+                },
               ),
-            );
-            if (sheet != null) {
-              print("Sheet : $sheet");
-            }
-          },
-        ),
-      ),
-    );
-  }
-}
-
-class CreditCardContainer extends StatelessWidget {
-  final double width;
-  final double height;
-  final double borderRadius;
-  final double bannerHeight;
-  final Widget? child;
-
-  const CreditCardContainer({
-    super.key,
-    this.width = 340,
-    this.height = 200,
-    this.borderRadius = 18,
-    this.bannerHeight = 40,
-    this.child,
-  });
-
-  @override
-  Widget build(BuildContext context) {
-    return SizedBox(
-      width: width,
-      height: height,
-      child: Container(
-        decoration: BoxDecoration(
-          color: Colors.white,
-          borderRadius: BorderRadius.circular(borderRadius),
-          border: Border.all(width: 2, color: Colors.grey.shade300),
-          boxShadow: [
-            BoxShadow(
-              blurRadius: 8,
-              offset: Offset(0, 4),
-              color: Colors.black.withValues(alpha: 0.10),
             ),
-          ],
-        ),
-        child: ClipRRect(
-          borderRadius: BorderRadius.circular(borderRadius - 2),
-          child: Column(
-            children: [
-              // Top Banner — ✅ No radius here!
-              Container(height: bannerHeight, color: Colors.grey.shade200),
-
-              // Body
-              Expanded(
-                child: Container(color: Colors.white, child: child),
-              ),
-            ],
           ),
-        ),
+        ],
       ),
     );
   }
